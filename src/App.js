bce1932a153d66443666bc6246419ea70e981aec
stpwin <sittiporn.k@codemonday.com>
2022-07-21 10:40:52 +0700

    initial

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [chunks, setChunks] = useState([]);
+
+  const fff = async () => {
+    fetch('http://localhost:9000/stream')
+      .then(async (response) => {
+        // response.body is a ReadableStream
+        const reader = response.body.getReader();
+        const textDecoder = new TextDecoder()
+        for await (const chunk of readChunks(reader)) {
+          const result = textDecoder.decode(chunk);
+          console.log(`received chunk of size ${chunk.length}`, result);
+          setChunks((prev) => [...prev, result]);
+        }
+      });
+
+    // readChunks() reads from the provided reader and yields the results into an async iterable
+    function readChunks(reader) {
+      return {
+        async*[Symbol.asyncIterator]() {
+          let readResult = await reader.read();
+          while (!readResult.done) {
+            yield readResult.value;
+            readResult = await reader.read();
+          }
+        },
+      };
+    }
+  }
+
+  useEffect(() => {
+    fff()
+    return () => { }
+  }, [])
+
+  return (
+    <div>
+      {chunks.map((chunk, index) => {
+        return <div key={index}>{chunk}</div>
+      })
+      }
+    </div>
+  );
+}
+
+export default App;
